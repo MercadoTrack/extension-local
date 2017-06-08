2895075001276d33e3c82bfa41c8b0e53075b8f7
GMaiolo <guille.maiolo@gmail.com>
2017-06-08 19:36:37 -0300

    still working on webpack bundling

--- build_scripts/copy.popup.js
+++
@@ -1,4 +0,0 @@
-let copy = require('copy')
-
-copy('./src/popup/popup.html', './dist/', { flatten: true }, () => console.log('Copied popup.html'))
-copy('./src/popup/popup.css', './dist/', { flatten: true }, () => console.log('Copied popup.css'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const paths = {
+    src: './src',
+    dist: `${__dirname}/dist`
+}
+
 module.exports = {
     entry: {
-        content: './src/scripts/content.js', 
-        background: './src/scripts/background.js', 
-        popup: './src/popup/popup.js'
+        content: `${paths.src}/scripts/content.js`,
+        background: `${paths.src}/scripts/background.js`,
+        popup: `${paths.src}/popup/popup.js`
     },
     output: {
-        path: __dirname + '/dist',
-        filename: "[name].bundle.js"
-    }
+        path: `${paths.dist}/scripts`,
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: `${paths.src}/popup/popup.html`, to: `${paths.dist}/popup` },
+            { from: `${paths.src}/popup/popup.css`, to: `${paths.dist}/popup` },
+            { from: './images', to: `${paths.dist}/images` },
+            { from: './manifest.json', to: paths.dist },
+            { from: './LICENSE', to: paths.dist },
+        ])
+    ]
 }
