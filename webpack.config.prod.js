c0b5db4abfa04a6e4b02a7d8d53c1f4b11f2fff4
GMaiolo <guille.maiolo@gmail.com>
2017-06-16 00:39:12 -0300

    added reminder to remove sourcemaps for prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 let webpackConf = require('./webpack.config')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+/* todo: remove sourcemaps for production */
+
 webpackConf.plugins = [new UglifyJSPlugin() , ...webpackConf.plugins]
 
 module.exports = webpackConf
