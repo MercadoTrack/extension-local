a64a08b9ad03dccefafcc5c4939160491a69b899
GMaiolo <guille.maiolo@gmail.com>
2020-07-14 09:01:26 -0300

    finished features

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,8 +1,9 @@
-import Utils from '../scripts/modules/utils/utils';
-import Vue from 'vue';
 import 'materialize-css';
 import './popup.sass';
+import Vue from 'vue';
 import axios from 'axios'
+import Utils from '../scripts/modules/utils/utils';
+import jwtDecode from 'jwt-decode'
 
 // "chrome-extension://dalhiaalbimlfakgphiamkjgaicfblen/popup.html"
 
@@ -12,59 +13,65 @@ Vue.filter('price', Utils.formatMoney)
 const AUTH_KEY = 'authResult'
 
 new Vue({
-    el: '#app',
-    data: () => ({
-        auth: JSON.parse(localStorage.getItem(AUTH_KEY)),
-        loading: false,
-        articles: [],
-        unfoldedArticle: null
-    }),
-    methods: {
-        login() {
-            this.loading = true // quizas authenticating en vez de loading?
-            chrome.runtime.sendMessage({
-                type: 'authenticate'
-            });
-            const checkAuthInterval = setInterval(() => {
-                const auth = JSON.parse(localStorage.getItem(AUTH_KEY))
-                if (auth) {
-                    this.auth = auth
-                    clearInterval(checkAuthInterval)
-                    this.getArticles()
-                }
-            }, 500)
-        },
-        getArticles() {
-            return axios.get('https://api.mercadotrack.com/user/favorites?full=true', { headers: { Authorization: `Bearer ${this.auth.id_token}` } })
-                .then(({ data }) => {
-                    this.articles = data
-                    console.log(data)
-                })
-                .catch((err) => {
-                    console.log(err.message)
-                    throw err
-                })
-        },
-        toggleArticle(article) {
-            this.unfoldedArticle = this.unfoldedArticle === article.id
-                ? null 
-                : article.id
-        },
-        isUnfolded(article) {
-            return this.unfoldedArticle === article.id
+  el: '#app',
+  data: () => ({
+    auth: JSON.parse(localStorage.getItem(AUTH_KEY)),
+    user: null,
+    authenticating: false,
+    loading: false,
+    articles: [],
+    unfoldedArticle: null
+  }),
+  methods: {
+    login() {
+      this.authenticating = true
+      chrome.runtime.sendMessage({
+        type: 'authenticate'
+      });
+      const checkAuthInterval = setInterval(() => {
+        const auth = JSON.parse(localStorage.getItem(AUTH_KEY))
+        if (auth) {
+          this.authenticating = false
+          this.auth = auth
+          this.loading = true
+          this.user = jwtDecode(auth.id_token)
+          this.getArticles().finally(() => {
+            this.loading = false
+          })
+          clearInterval(checkAuthInterval)
         }
+      }, 1000)
     },
-    created () {
-        if (this.auth) {
-            this.loading = true
-            this.getArticles()
-                .then(() => {
-                    this.loading = false
-                })
-                .catch((err) => {
-                    console.log(err)
-                    this.loading = false
-                })
-        }
+    getArticles() {
+      return axios.get('https://api.mercadotrack.com/user/favorites?full=true', { headers: { Authorization: `Bearer ${this.auth.id_token}` } })
+      .then(({ data }) => {
+        this.articles = data
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+        throw err
+      })
+    },
+    getArticleLink(article) {
+      return `https://mercadotrack.com/articulo/${article.id}`
+    },
+    toggleArticle(article) {
+      this.unfoldedArticle = this.unfoldedArticle === article.id
+      ? null 
+      : article.id
+    },
+    isUnfolded(article) {
+      return this.unfoldedArticle === article.id
     }
+  },
+  created () {
+    if (this.auth) {
+      this.user = jwtDecode(this.auth.id_token)
+      this.loading = true
+      this.getArticles().finally(() => {
+        this.loading = false
+      })
+    }
+  }
 })
---
+++ src/scripts/background.js
@@ -0,0 +1,34 @@
+chrome.runtime.onMessage.addListener(function (event) {
+  if (event.type === 'authenticate') {
+ 
+    // scope
+    //  - openid if you want an id_token returned
+    //  - offline_access if you want a refresh_token returned
+    // device
+    //  - required if requesting the offline_access scope.
+    let options = {
+      scope: 'openid profile email offline_access',
+      device: 'chrome-extension'
+    };
+ 
+    new Auth0Chrome(env.AUTH0_DOMAIN, env.AUTH0_CLIENT_ID)
+      .authenticate(options)
+      .then(function (authResult) {
+        localStorage.authResult = JSON.stringify(authResult);
+        // chrome.notifications.create({
+        //   type: 'basic',
+        //   iconUrl: 'images/icon_128.png',
+        //   title: 'Login Successful',
+        //   message: 'You can use the app now'
+        // });
+      }).catch(function (err) {
+        console.log(err)
+        // chrome.notifications.create({
+        //   type: 'basic',
+        //   title: 'Login Failed',
+        //   message: err.message,
+        //   iconUrl: 'images/icon_128.png'
+        // });
+    });
+  }
+});
